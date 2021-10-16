0d4177740610c2c1a14674011dd6ca4ebd7fb54f
Ali AHmed <m.aliahmed0@gmail.com>
2021-10-16 08:42:57 +0500

    sd

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -22,118 +22,110 @@
                 </div>
 
 
-                <div class="row">
-                    <div class="col-12 table-responsive">
-                        <table class="table table-striped">
-                            <thead>
-                            <tr>
-                                <th>
+                <div class="col-12 table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                        <tr>
+                            <th>
 
 
-                                    <h1><strong>Amroza.com</strong></h1>
+                                <h1><strong>Amroza.com</strong></h1>
+                                <br>
+
+
+                                <h3><b>Invoice #:{{$invoice['invoiceId']}}
+                                    </b>
                                     <br>
+                                    <br>
+                                    <b>Order #: {{$invoice['order']['orderId']}} </b><br>
+                                </h3>
+                            </th>
+
+                            <th>
+
+                                Name: {{$invoice['order']['customer']['name']}}<br>
+                                Phone: {{$invoice['order']['customer']['phone']}}<br>
+                                Address: {{$invoice['order']['customer']['address']}}<br>
+                                Store Name: {{$invoice['order']['customer']['storeName']}}
+                                <br>
+                                Store Type: {{$invoice['order']['customer']['storeType']}}
+                                <br>
 
 
-                                    <h3><b>Invoice #:{{$invoice['invoiceId']}}
-                                        </b>
-                                        <br>
-                                        <br>
-                                        <b>Order #: {{$invoice['order']['orderId']}} </b><br>
-                                    </h3>
-                                </th>
-                                <th></th>
-
-                                <th>
-                                    <strong>To:</strong>
-                                    <address>
-                                        <strong>Name: </strong>{{$invoice['order']['customer']['name']}}<br>
-                                        <strong>Phone: </strong> {{$invoice['order']['customer']['phone']}}<br>
-                                        <strong>Address: </strong> {{$invoice['order']['customer']['address']}}<br>
-                                        <strong>Store Name: </strong> {{$invoice['order']['customer']['storeName']}}
-                                        <br>
-                                        <strong>Store Type: </strong> {{$invoice['order']['customer']['storeType']}}
-                                        <br>
-                                    </address>
-
-                                </th>
+                            </th>
 
 
-                            </tr>
+                        </tr>
+                        <tr>
+                            <th>
+                                <center>Sr #</center>
+                            </th>
+                            <th>Product Name</th>
+                            <th>
+                                <center>Price</center>
+                            </th>
+                            <th>
+                                <center>Quantity</center>
+                            </th>
+                            <th>
+                                <center>Total</center>
+                            </th>
+
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        @php($count=1)
+                        @foreach($invoice['order']['countModelArrayList'] as $item)
                             <tr>
-                                <th>
-                                    <center>Sr #</center>
-                                </th>
-                                <th>Product Name</th>
-                                <th>
-                                    <center>Price</center>
-                                </th>
-                                <th>
-                                    <center>Quantity</center>
-                                </th>
-                                <th>
-                                    <center>Total</center>
-                                </th>
+                                <td style="padding: 0rem;width: 20px;">
+                                    <center>{{$count}}</center>
+                                </td>
+                                <td style="padding: 0rem;width:500px">{{$item['product']['title']}}</td>
+                                <td style="padding: 0rem;">
+                                    <center>{{$item['product']['price']}}</center>
+                                </td>
+                                <td style="padding: 0rem;">
+                                    <center>{{$item['quantity']}}</center>
+                                </td>
+                                <td style="padding: 0rem;">
+                                    <center>{{$item['quantity']*$item['product']['price']}}</center>
+                                </td>
+
 
                             </tr>
-                            </thead>
-                            <tbody>
-
-                            @php($count=1)
-                            @foreach($invoice['order']['countModelArrayList'] as $item)
-                                <tr>
-                                    <td style="padding: 0rem;width: 20px;">
-                                        <center>{{$count}}</center>
-                                    </td>
-                                    <td style="padding: 0rem;width:500px">{{$item['product']['title']}}</td>
-                                    <td style="padding: 0rem;">
-                                        <center>{{$item['product']['price']}}</center>
-                                    </td>
-                                    <td style="padding: 0rem;">
-                                        <center>{{$item['quantity']}}</center>
-                                    </td>
-                                    <td style="padding: 0rem;">
-                                        <center>{{$item['quantity']*$item['product']['price']}}</center>
-                                    </td>
-
-
-                                </tr>
-                                @php($count++)
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                    <!-- /.col -->
+                            @php($count++)
+                        @endforeach
+                        </tbody>
+                    </table>
                 </div>
-                <!-- /.row -->
-
-                <div class="row">
-                    <div class="col-12">
-                        <!-- accepted payments column -->
-                        <hr>
-                        <h3 class="float-right mr-5">Total: Rs {{$invoice['order']['totalPrice']}}</h3>
-
-                    </div>
-
-                </div>
+                <!-- /.col -->
             </div>
+            <!-- /.row -->
 
-            <div class="row no-print">
+            <div class="row">
                 <div class="col-12">
-                    <button onclick="window.print()" target="_blank" class="btn btn-default"><i
-                                class="fas fa-print"></i> Print
-                    </button>
-
+                    <!-- accepted payments column -->
+                    <hr>
+                    <h3 class="float-right mr-5">Total: Rs {{$invoice['order']['totalPrice']}}</h3>
 
                 </div>
+
             </div>
         </div>
 
-    </div>
-    {{--order details end--}}
+        <div class="row no-print">
+            <div class="col-12">
+                <button onclick="window.print()" target="_blank" class="btn btn-default"><i
+                            class="fas fa-print"></i> Print
+                </button>
 
-    {{--user details--}}
 
+            </div>
+        </div>
     </div>
+
+
 @stop
 
 @section('css')
